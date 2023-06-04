60ef7303d53c78ed9686bc6c48e40a2d951d971d
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-06-04 14:47:15 +0530

    added complex queries

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Database Management System (MySQL)
 <p align="center">
-  <img src="./Not.png" alt="Image">
+  <!-- <img src="./Not.png" alt="Image"> -->
+  Almost there Everything seems to be working perfect.
+  All Database Management System programs are flying to their limits.
 </p>
--- example_12.sql
+++ example_12.sql
@@ -24,37 +24,22 @@ INSERT INTO Orders VALUES (3, 3, '2023-05-25');
 INSERT INTO Orders VALUES (4, 2, '2023-05-30');
 INSERT INTO Orders VALUES (5, 4, '2023-06-01');
 
--- customers who have orders in the last 30 days
-SELECT c.customer_name
-FROM Customers c
-INNER JOIN Orders o ON c.customer_id = o.customer_id
-WHERE o.order_date >= CURDATE() - INTERVAL 30 DAY;
+-- join
+SELECT Customers.customer_name, Orders.order_id, Orders.order_date
+FROM Customers
+INNER JOIN Orders ON Customers.customer_id = Orders.customer_id;
 
--- total number of orders by each customer
-SELECT c.customer_name, COUNT(o.order_id) AS total_orders
-FROM Customers c
-LEFT JOIN Orders o ON c.customer_id = o.customer_id
-GROUP BY c.customer_id, c.customer_name;
-
--- customers not placed any orders
-SELECT c.customer_name
-FROM Customers c
-LEFT JOIN Orders o ON c.customer_id = o.customer_id
-WHERE o.order_id IS NULL;
-
--- customers from either India or USA
+-- subquery
 SELECT customer_name
 FROM Customers
-WHERE country IN ('India', 'USA');
+WHERE customer_id IN (SELECT customer_id FROM Orders WHERE order_date > '2023-01-01');
 
--- customers from India who have placed orders
-SELECT c.customer_name
-FROM Customers c
-INNER JOIN Orders o ON c.customer_id = o.customer_id
-WHERE c.country = 'India';
+-- aggregation
+SELECT customer_id, COUNT(order_id) AS order_count
+FROM Orders
+GROUP BY customer_id;
 
--- customers from India who not placed any orders
-SELECT c.customer_name
-FROM Customers c
-LEFT JOIN Orders o ON c.customer_id = o.customer_id
-WHERE c.country = 'India' AND o.order_id IS NULL;
+--union 
+SELECT customer_name FROM Customers
+UNION
+SELECT order_id FROM Orders;
