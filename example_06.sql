dc1bd233880660c287dbb5024f98fd4d275aa80e
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-06-04 14:34:31 +0530

    Added cursor program

--- example_06.sql
+++ example_06.sql
@@ -1,68 +1,34 @@
--- I am working on this cursor program keep patience
-
-
--- code below is broken
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- Write a PL/SQL block to implement all types of cursor
 
 
 -- Create the Student table
 CREATE TABLE Student (
     id INT PRIMARY KEY,
     name VARCHAR(20),
-    grade VARCHAR(3)
+    grade float
 );
 
+-- Insert some mock data
+ INSERT INTO Student VALUES (1, 'Om', 90.45);
+ INSERT INTO Student VALUES (2, 'Vaibhav', 85.75);
+ INSERT INTO Student VALUES (3, 'Vaishnavi', 96.23);
+ INSERT INTO Student VALUES (4, 'Samadhan', 82.56);
 
-DECLARE
-   -- Implicit Cursor
-   CURSOR c1 IS
-      SELECT id, name, grade
-      FROM Student;
-      
-   -- Explicit Cursor (UPDATE statement)
-   CURSOR c2 IS
-      SELECT id, grade
-      FROM Student
-      WHERE grade = 'C';
-      
-   -- Variable declarations
-   v_id int;
-   v_name varchar(20);
-   v_grade varchar(3);
-BEGIN
-   -- Implicit Cursor (SELECT query)
-   FOR std IN c1 LOOP
-      DBMS_OUTPUT.PUT_LINE('Student ID: ' || std.id || ', Name: ' || std.name || ', Grade: ' || std.grade);
-   END LOOP;
-   
-   -- Explicit Cursor (UPDATE statement)
-   OPEN c2;
-   LOOP
-      FETCH c2 INTO v_id, v_grade;
-      EXIT WHEN c2%NOTFOUND;
-      v_grade := 'B'; -- Update the grade to 'B'
-      UPDATE Student
-      SET grade = v_grade
-      WHERE id = v_id;
-   END LOOP;
-   CLOSE c2;
+-- create procedure containing cursor
+delimiter //
+CREATE PROCEDURE pcursor()
+ BEGIN
+     DECLARE s_id INT;
+     DECLARE s_name VARCHAR(20);
+     DECLARE cur1 CURSOR FOR SELECT id, name FROM Student WHERE grade > 85;
+     OPEN cur1;
+     read_loop: LOOP
+         FETCH cur1 INTO s_id, s_name;
+         SELECT CONCAT(s_id, ' ', s_name) AS info;
+     END LOOP;
+     CLOSE cur1;
+ END;
+//
 
-EXCEPTION
-   WHEN OTHERS THEN
-END;
-/
+-- call procedure
+call pcursor() //
